d6e638b959207d73390c9f70617b8e0e8698cab6
Fortuneod <fortuneodesanya@gmail.com>
2016-10-10 11:42:13 +0100

    Update deleteitem.php

--- deleteitem.php
+++ deleteitem.php
@@ -3,27 +3,7 @@
 	
 	if(!empty($_POST['delid'])) {
 			
-		$delid=$_POST['delid'];
-		
-		$ch="SELECT count(*) as rownum FROM `inventory` WHERE itemid='$delid'";
-		$check=$conn->getData($ch);
-		$rn=$conn->Display();
-		$rownum=$rn['rownum'];
-		
-		if($rownum>0) {		
-		
-		$sql="DELETE FROM `inventory` WHERE itemid='$delid'";
-		$run=$conn->Execute($sql);
-			
-		if($run) {
-			
-			echo "<h2>Item Removed Successfully!</h2>";
-		}
-		else
-			echo "<h2 style='color:red;'>An Error Occured. Try Again!</h2>";				
-		}
-		else
-			echo "<h2 style='color:red;'>No Match Found For that ID</h2>";	
+		$conn->DeleteItem();	
 					
 	}
 	else 
