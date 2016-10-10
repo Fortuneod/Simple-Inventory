25fd0e97b7bfb0a25e315ce9ef36d54949a6cdb1
Fortuneod <fortuneodesanya@gmail.com>
2016-10-10 11:44:35 +0100

    Update newitem.php

--- newitem.php
+++ newitem.php
@@ -3,19 +3,7 @@
 	
 	if(!empty($_POST['itemid']) && !empty($_POST['describe']) && !empty($_POST['quantity']) && !empty($_POST['acquired']) && !empty($_POST['span']) && !empty($_POST['depreciate']) && !empty($_POST['money'])) {
 	
-	$id=rand(1, 100000);
-	$item=$_POST['itemid'];
-	$describe=$_POST['describe'];
-	$quantity=$_POST['quantity'];
-	$acquired=$_POST['acquired'];
-	$span=$_POST['span'];
-	$depreciate=$_POST['depreciate'];
-	$money=$_POST['money'];
-	
-	$money=preg_replace("/[^0-9]/", "", $money);
-	$span=preg_replace("/[^0-9]/", "", $span);
-	$depreciate=preg_replace("/[^0-9]/", "", $depreciate);
-	
+		
 	$tb="CREATE TABLE IF NOT EXISTS `inventory` (
 	`id` int(11) not null,
 	`itemid` varchar(10) unique not null,
@@ -31,29 +19,12 @@
 	
 	$cr=$conn->Execute($tb);
 	
-	$ch="SELECT count(*) as rownum FROM `inventory` WHERE itemid='$item'";
-	$check=$conn->getData($ch);
-	$nb=$conn->Display();
-	$count=$nb['rownum'];
+	$conn->NewData();
 	
-	if($count>0){
-		$itemid=rand(1, 100000);
-	}
-	else {
-		$itemid=$item;
-	}
-	
-	$sql="INSERT INTO `inventory` VALUES('$id', '$itemid', '$describe', '$quantity', '$acquired', '$span', '$depreciate', '$money')";
-	$run=$conn->Execute($sql);
-	
-	if($run) {
-		echo "<p style='color:#fff;font-size:20px;'>Item Added To Inventory Successfully!</p>";
-	}
-	else
-		echo "<p style='color:red;'>Sorry!!! An Error Occurred. Try Again!</p>";
 	}
 	else
 		echo "<p style='color:red;'>One Or More Fields Left Blank</p>";
+	
 	
 ?>
 	
